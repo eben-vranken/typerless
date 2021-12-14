434978485d5a2bdaa9611e68515efb97bc2c1c60
d15gu15e <d15gu15s@protonmail.com>
2021-12-14 22:38:02 +0100

    first-build

---
+++ scripts/script.js
@@ -0,0 +1,81 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const API_URL = "http://api.quotable.io/random";
+const navBtn = document.querySelector(".nav-btn");
+const quoteElement = document.querySelector(".quote");
+const quoteInput = document.querySelector(".quote-input");
+// Don't allow selection of the input
+quoteInput.addEventListener("select", function () {
+    this.selectionStart = this.selectionEnd;
+}, false);
+// Respond to person typing
+quoteInput.addEventListener("input", () => {
+    var _a;
+    const lettersArray = quoteElement.querySelectorAll("span");
+    const typedValue = quoteInput.value.split("");
+    // Add cursor
+    let correct = true;
+    lettersArray.forEach((charElement, i) => {
+        var _a;
+        // Remove active class
+        (_a = quoteElement.children[i]) === null || _a === void 0 ? void 0 : _a.classList.remove("active");
+        const char = typedValue[i];
+        // Prevent future characters & removing characters
+        if (char == null) {
+            charElement.classList.remove("correct");
+            charElement.classList.remove("incorrect");
+            correct = false;
+        }
+        else if (char === charElement.innerText) {
+            charElement.classList.add("correct");
+            charElement.classList.remove("incorrect");
+        }
+        else {
+            charElement.classList.remove("correct");
+            charElement.classList.add("incorrect");
+            correct = false;
+        }
+    });
+    const index = typedValue.length;
+    if (correct) {
+        getRandomQuote();
+    }
+    else {
+        (_a = quoteElement.children[index]) === null || _a === void 0 ? void 0 : _a.classList.add("active");
+    }
+});
+const apiCall = function () {
+    return fetch(API_URL)
+        .then((res) => res.json())
+        .then((data) => data.content);
+};
+function getRandomQuote() {
+    return __awaiter(this, void 0, void 0, function* () {
+        // Clear input
+        quoteInput.value = "";
+        quoteElement.textContent = "";
+        // Get Quote
+        const quote = yield apiCall();
+        // Turn every quote character into span element
+        quote.split("").forEach((c) => {
+            const spanElement = document.createElement("span");
+            spanElement.innerText = c;
+            quoteElement.appendChild(spanElement);
+        });
+    });
+}
+// Navbar open and closing
+navBtn.addEventListener("click", function () {
+    const navItems = document.querySelector(".nav-items");
+    navItems.classList.toggle("open");
+    navBtn.classList.toggle("open");
+});
+getRandomQuote();
---
+++ scripts/script.ts
@@ -0,0 +1,74 @@
+const API_URL = "http://api.quotable.io/random";
+const navBtn: HTMLButtonElement = document.querySelector(".nav-btn");
+const quoteElement: HTMLElement = document.querySelector(".quote");
+const quoteInput: HTMLInputElement = document.querySelector(".quote-input");
+
+// Don't allow selection of the input
+quoteInput.addEventListener(
+  "select",
+  function () {
+    this.selectionStart = this.selectionEnd;
+  },
+  false
+);
+
+// Respond to person typing
+quoteInput.addEventListener("input", () => {
+  const lettersArray: NodeListOf<HTMLSpanElement> =
+    quoteElement.querySelectorAll("span");
+  const typedValue: Array<string> = quoteInput.value.split("");
+  // Add cursor
+  let correct = true;
+  lettersArray.forEach((charElement, i) => {
+    // Remove active class
+    quoteElement.children[i]?.classList.remove("active");
+    const char = typedValue[i];
+    // Prevent future characters & removing characters
+    if (char == null) {
+      charElement.classList.remove("correct");
+      charElement.classList.remove("incorrect");
+      correct = false;
+    } else if (char === charElement.innerText) {
+      charElement.classList.add("correct");
+      charElement.classList.remove("incorrect");
+    } else {
+      charElement.classList.remove("correct");
+      charElement.classList.add("incorrect");
+      correct = false;
+    }
+  });
+
+  quoteElement.children[typedValue.length]?.classList.add("active");
+  if (correct) {
+    getRandomQuote();
+  }
+});
+
+const apiCall = function () {
+  return fetch(API_URL)
+    .then((res: Response) => res.json())
+    .then((data) => data.content);
+};
+
+async function getRandomQuote() {
+  // Clear input
+  quoteInput.value = "";
+  quoteElement.textContent = "";
+  // Get Quote
+  const quote: string = await apiCall();
+  // Turn every quote character into span element
+  quote.split("").forEach((c: string) => {
+    const spanElement: HTMLElement = document.createElement("span");
+    spanElement.innerText = c;
+    quoteElement.appendChild(spanElement);
+  });
+}
+
+// Navbar open and closing
+navBtn.addEventListener("click", function () {
+  const navItems = document.querySelector(".nav-items");
+  navItems.classList.toggle("open");
+  navBtn.classList.toggle("open");
+});
+
+getRandomQuote();
